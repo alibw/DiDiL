48d7a3e91c71a3e0a6a94d414043fa9302558651
alibw <69758323+alibw@users.noreply.github.com>
2023-04-16 11:39:19 +0330

    Added : Length & equality members

--- Model/Column.cs
+++ Model/Column.cs
@@ -1,9 +1,11 @@
 ﻿namespace Model;
 
-public class Column
+public class Column : IEquatable<Column>
 {
     public string Name { get; set; }
     public string Type { get; set; }
+    
+    public string Length { get; set; }
     public bool Nullable { get; set; }
 
     public bool IsIdentity { get; set; }
@@ -11,6 +13,27 @@ public class Column
     public bool IsPrimaryKey { get; set; }
 
     public bool IsForeignKey { get; set; }
-    
+
     public Table? ForeignKeyTable { get; set; }
+
+
+    public bool Equals(Column? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Name == other.Name && Type == other.Type && Nullable == other.Nullable && IsIdentity == other.IsIdentity && IsPrimaryKey == other.IsPrimaryKey && IsForeignKey == other.IsForeignKey && Equals(ForeignKeyTable, other.ForeignKeyTable);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Column)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name, Type, Nullable, IsIdentity, IsPrimaryKey, IsForeignKey, ForeignKeyTable);
+    }
 }
